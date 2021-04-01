d036868ad6edcef63bcad43f4e65af74374dbda5
Luiz Viana <luiz.viana@mongodb.com>
2021-04-01 18:15:25 +0100

    Generate audit log entries for each request

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ _This project requires Drone server version 1.4 or higher._
 
 Create a shared secret:
 
-```console
-$ openssl rand -hex 16
+```shell
+openssl rand -hex 16
 bea26a2221fd8090ea38720fc445eca6
 ```
 
 Download and run the plugin:
 
-```console
-$ docker run -d \
+```shell
+docker run -d \
   --publish=3000:3000 \
   --env=DRONE_DEBUG=true \
   --env=DRONE_SECRET=bea26a2221fd8090ea38720fc445eca6 \
@@ -24,13 +24,23 @@ $ docker run -d \
 
 If you are deploying locally using helm:
 
-```console
-$ helm install -n drone-validation -f ./environment/dev.yaml mongodb/web-app
+```shell
+helm install -n drone-validation -f ./environment/dev.yaml mongodb/web-app
 ```
 
 Update your Drone server environment variables to include the plugin address and the shared secret.
 
-```text
+```shell
 DRONE_VALIDATE_PLUGIN_ENDPOINT=http://<service-name>:3000
 DRONE_VALIDATE_PLUGIN_SECRET=bea26a2221fd8090ea38720fc445eca6
+```
+
+## Kanopy Deployment
+
+Access the dronev1 namespace in the Kanopy cluster where you want to enable the validation extension.
+
+Create a new value in drone-secrets with the validate plugin secret:
+
+``` shell
+ksec set drone-secrets DRONE_VALIDATE_PLUGIN_SECRET=a750ac1f1a411197d7ae930cd5d01fc
 ```
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -8,9 +8,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/drone/drone-go/plugin/validator"
 	"github.com/open-policy-agent/opa/rego"
+	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 )
 
@@ -21,28 +24,28 @@ func New(policy string) validator.Plugin {
 	}
 }
 
-type plugin struct {
-	policyPath string
-}
-
 func (p *plugin) Validate(ctx context.Context, req *validator.Request) error {
-	// Drone sends its raw config in the request body
-	type DroneConfig struct {
-		Kind  string `yaml:"kind" json:"kind"`
-		Type  string `yaml:"type" json:"type"`
-		Name  string `yaml:"name" json:"name"`
-		Steps []struct {
-			Name      string   `yaml:"name" json:"name"`
-			Image     string   `yaml:"image" json:"image"`
-			Commands  []string `yaml:"commands" json:"commands"`
-			DependsOn []string `yaml:"depends_on,omitempty" json:"depends_on,omitempty"`
-		} `yaml:"steps" json:"steps"`
-		Trigger struct {
-			Branch []string `yaml:"branch" json:"branch"`
-		} `yaml:"trigger" json:"trigger"`
-	}
 
 	var droneConfig DroneConfig
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+
+	promotion := Promotion{
+		Build:       req.Build.Parent,
+		Environment: req.Build.Deploy,
+	}
+
+	// log each request received for audit purposes
+	logrus.WithFields(logrus.Fields{
+		"extension":  "validation",
+		"user":       req.Build.Sender,
+		"event":      req.Build.Event,
+		"repository": req.Repo.Slug,
+		"branch":     req.Repo.Branch,
+		"commit":     req.Build.After,
+		"promotion":  &promotion,
+	}).Info(fmt.Sprintf("audit(%d)", time.Now().Unix()))
 
 	err := yaml.Unmarshal([]byte(req.Config.Data), &droneConfig)
 	if err != nil {
@@ -65,7 +68,7 @@ func (p *plugin) Validate(ctx context.Context, req *validator.Request) error {
 
 	if rs[0].Bindings["deny"] == true {
 		message := fmt.Sprintf("[validator] %v", rs[0].Bindings["msg"])
-	 	return errors.New(message)
+		return errors.New(message)
 	}
 
 	if err != nil {
